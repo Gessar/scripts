c3de2cb4aaf391164d50a31c0eb2e9bdf23a97a3
Gessar <Gessar2010@gmail.com>
2024-04-30 12:04:37 +0500

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -20,4 +20,4 @@ for apps in docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-co
 #sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 sudo usermod -aG docker $USER
-exec su -l $USER
+newgrp docker
